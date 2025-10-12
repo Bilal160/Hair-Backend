205fc477bbaeec39b5ba634358fcb6eb53981a4b
Mather Tahir <mather@techtix.co>
2025-10-12 15:03:42 +0500

    Auyth comment

--- index.ts
+++ index.ts
@@ -11,13 +11,13 @@ import { sendErrorResponse } from "./src/utils/responseUtils";
 
 const app = express();
 
-app.use(cors(
-  {
+app.use(
+  cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }
-));
+  })
+);
 
 const server = createServer(app);
 
@@ -36,7 +36,6 @@ mongoConnect()
     console.error(error);
   });
 
-// Test
 const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`Express app is running on port ${port}`);
